a15de87044f772fe9342d0675e9533beb8518b51
hugopinto1997 <hugopillo@hotmail.com>
2018-06-15 23:32:47 -0600

    The start of the end

---
+++ app/src/main/java/com/hugopinto/segundoparcial/Activities/PlayerFullView.java
@@ -0,0 +1,20 @@
+package com.hugopinto.segundoparcial.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hugopinto.segundoparcial.R;
+
+
+public class PlayerFullView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_full_view);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+
+    }
+}
